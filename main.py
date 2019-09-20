e6a16b98359ae01ad5a05b8840dd54f550cd1b29
BuildTools <unconfigured@null.spigotmc.org>
2019-09-19 20:51:09 -0400

    Message

---
+++ main.py
@@ -0,0 +1,16 @@
+class main():
+
+    def __init__(self):
+        self.structure = []
+
+    def readCSV(self):
+        file = open("FILEURLHERE", "w");
+        
+
+
+
+
+
+
+
+        file.close()
