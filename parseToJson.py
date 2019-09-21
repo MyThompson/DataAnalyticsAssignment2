9ca3039821dd54bc5717a9d7d424d21394e44c6f
Mercedes Y Thompson <mythompson@bsu.edu>
2019-09-21 00:43:43 -0400

    file that parses to json but probably not clean

---
+++ parseToJson.py
@@ -0,0 +1,29 @@
+import xlrd
+import json
+
+excel = xlrd.open_workbook("Project2DataTrimmed.xlsx")
+sheet = excel.sheet_by_index(0)
+
+data={}
+temp={}
+data['data'] = []
+for i in range(0,sheet.nrows):
+    for j in range(0,sheet.ncols):
+        # print (sheet.cell(i,j).value)
+        temp['{}'.format(sheet.cell(0,j).value)]=sheet.cell(i,j).value
+
+    if i > 0 :
+        tempList = [(k,v) for k,v in temp.items()]
+        data['data']=data['data']+tempList
+print (data['data'])
+
+
+
+
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+
+# print (data.sheet_names())
+
+# print ("hello world")
