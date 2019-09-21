dbe38aaa72b835655641d6e88af1d5067def3012
BuildTools <unconfigured@null.spigotmc.org>
2019-09-21 01:00:24 -0400

    Adds CSV to data structure

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-class main():
+class CSV:
+
+    structure = {}
+    keylist = []
 
     def __init__(self):
-        self.structure = []
+        self.structure = {}
+        self.keylist = []
 
-    def readCSV(self):
-        file = open("FILEURLHERE", "w");
-        
+    def read_csv(self):
+        file = open("/home/dalton/Documents/School_Stuff/CS421/Data.csv", "r");
+        for x in file.readline().split(","):
+            self.structure.update({x: []})
+            self.keylist.append(x)
+
+        for lines in file:
+            array = lines.split(",")
+            for i in range(len(self.keylist)):
+                self.structure[self.keylist[i]].append(array[i])
 
 
+def main():
+    test = CSV
+    test.read_csv(CSV)
 
 
-
-
-
-        file.close()
+if __name__ == '__main__':
+    main()
