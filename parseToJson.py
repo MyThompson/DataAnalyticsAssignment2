c6b76b3b3cba8a5d11cfed148d207c7daadc8f44
Mercedes Y Thompson <mythompson@bsu.edu>
2019-09-23 09:47:47 -0400

    csv parses to json

--- parseToJson.py
+++ parseToJson.py
@@ -1,29 +1,43 @@
 import xlrd
 import json
 
-excel = xlrd.open_workbook("resources/Project2DataTrimmed.xlsx")
-sheet = excel.sheet_by_index(0)
+# excel = xlrd.open_workbook("resources/Project2DataTrimmed.xlsx")
+# sheet = excel.sheet_by_index(0)
 
-data={}
-temp={}
-data['data'] = []
-for i in range(0,sheet.nrows):
-    for j in range(0,sheet.ncols):
-        # print (sheet.cell(i,j).value)
-        temp['{}'.format(sheet.cell(0,j).value)]=sheet.cell(i,j).value
+# data={}
+# temp={}
+# data['data'] = []
+# for i in range(0,sheet.nrows):
+#     for j in range(0,sheet.ncols):
+#         # print (sheet.cell(i,j).value)
+#         temp['{}'.format(sheet.cell(0,j).value)]=sheet.cell(i,j).value
 
-    if i > 0 :
-        tempList = [(k,v) for k,v in temp.items()]
-        data['data']=data['data']+tempList
+#     if i > 0 :
+#         tempList = [(k,v) for k,v in temp.items()]
+#         data['data']=data['data']+tempList
 # print (data['data'])
 
 
+keylist = []
+temp={}
+data={}
+data['data']=[]
+
+file = open("resources/Project2DataTrimmed.csv", "r")
+for x in file.readline().split(","):
+    keylist.append(x)
+
+for lines in file:
+    array = lines.split(",")
+    for i in range(len(keylist)):
+        temp['{}'.format(keylist[i])]=array[i]
+        
+
+    tempList = [(k,v) for k,v in temp.items()]
+    data['data']=data['data']+tempList
+print(data['data'])
 
 print(json.dumps(data))
 with open('data.json', 'w') as outfile:
     json.dump(data, outfile)
 
-
-# print (data.sheet_names())
-
-# print ("hello world")
